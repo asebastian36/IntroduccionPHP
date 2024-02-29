0f28cccec4cb99671818b26474fc92e687a62af5
asebastian36 <asebastianjob@gmail.com>
2024-02-28 22:44:33 -0600

    se me paso subir los cambios

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -1,1 +1,11 @@
-<h1>Hola mundo en PHP</h1>
+<?php include 'includes/header.php';//  para llamar otros scripts de php
+
+//  maneras de usar la salida estandar
+echo"Hola desde PHP \t";
+print "otro mensaje\t";
+print_r("Otro otro mensaje...\t");
+var_dump("Hola mundo");
+
+//  otro script de php
+include 'includes/footer.php';
+
--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,18 @@
 <?php include 'includes/header.php';
 
+//  creacion de variables
+$_variable = 'Hola mundo';// si puede llevar guiones el nombre al inicio o al final
+var_dump($_variable);
 
+//  para constantes
+define('constante', 'Soy el valor de la constante');
+var_dump(constante);
 
+const pi = 3.1416;
+var_dump(pi);
+
+//  sintaxis mas comun
+$nombre_cliente = "PALALALA";
+$nombreCliente = 'PALALALA';
 
 include 'includes/footer.php';
